d8d6e67ac584e241b65a9d63507e43d779aacdd9
BunniKaitlyn <ItsKaitlyn03@protonmail.com>
2024-06-05 11:56:20 -0400

    Fix MenuButtonManager on 1.29.0

--- OverlySensitiveSpectrograms/Managers/MenuButtonManager.cs
+++ OverlySensitiveSpectrograms/Managers/MenuButtonManager.cs
@@ -4,40 +4,47 @@ using BeatSaberMarkupLanguage;
 using BeatSaberMarkupLanguage.MenuButtons;
 using OverlySensitiveSpectrograms.UI;
 using System;
+#if !LATEST
+using System.Threading;
+using System.Threading.Tasks;
+#endif
 using Zenject;
 
 namespace OverlySensitiveSpectrograms.Managers;
 
 internal class MenuButtonManager : IInitializable, IDisposable
 {
-    private readonly MainFlowCoordinator _mainFlowCoordinator;
-    private readonly OSSFlowCoordinator _ossFlowCoordinator;
-    private readonly MenuButtons _menuButtons;
+    [Inject] private readonly MainFlowCoordinator _mainFlowCoordinator = null!;
+    [Inject] private readonly OSSFlowCoordinator _ossFlowCoordinator = null!;
+#if LATEST
+    [Inject] private readonly MenuButtons _menuButtons = null!;
+#endif
 
     private MenuButton _menuButton = null!;
 
-    public MenuButtonManager(
-        MainFlowCoordinator mainFlowCoordinator,
-        OSSFlowCoordinator ossFlowCoordinator,
-        MenuButtons menuButtons)
+    public async void Initialize()
     {
-        _mainFlowCoordinator = mainFlowCoordinator;
-        _ossFlowCoordinator = ossFlowCoordinator;
-        _menuButtons = menuButtons;
-    }
+        _menuButton = new MenuButton("<size=80%>Overly Sensitive Spectrograms", "These spectrograms wildin' over here.", MenuButton_onClick);
 
-    public void Initialize()
-    {
-        _menuButton = new MenuButton("<size=80%>Overly Sensitive Spectrograms", "These spectrograms wildin' over here.", OnMenuButtonClick);
+#if LATEST
         _menuButtons.RegisterButton(_menuButton);
+#else
+        await Task.Run(() => Thread.Sleep(100));
+        MenuButtons.instance.RegisterButton(_menuButton);
+#endif
     }
 
     public void Dispose()
     {
+#if LATEST
         _menuButtons.UnregisterButton(_menuButton);
+#else
+        if (BSMLParser.IsSingletonAvailable && MenuButtons.IsSingletonAvailable)
+            MenuButtons.instance.UnregisterButton(_menuButton);
+#endif
     }
 
-    private void OnMenuButtonClick()
+    private void MenuButton_onClick()
     {
         _mainFlowCoordinator.PresentFlowCoordinator(_ossFlowCoordinator);
     }
